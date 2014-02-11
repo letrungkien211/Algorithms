c8c7505c596105fb94cd46d8a14d25b84c7b59bc
letrungkien211 <letrungkien211@gmail.com>
2014-02-11 18:29:32 +0900

    Done sort list

--- Leetcode/SortList/main.cpp
+++ Leetcode/SortList/main.cpp
@@ -1,3 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+
+Sort a linked list in O(n log n) time using constant space complexity.
+ */
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -12,94 +22,94 @@
 
 using namespace std;
 
-#define For(i,m,n) for(int i=m; i < n; i++)
-
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
-    ListNode(int x, ListNode* node): val(x), next(node){}
 };
 
-void push_back(ListNode** current, int val){
-    ListNode* newnode = new ListNode(val, NULL);
-    if(!*current){
-	*current = newnode;
-    }
-    else{
-	ListNode* it = *current;
-	while(it->next)
-	    it=it->next;
-	it->next = newnode;
-    }
-}
 
-void push_front(ListNode** current, int val){
-    *current = new ListNode(val, *current);
-}
+#define For(i,m,n) for(int i=m; i < n; i++)
 
+class Solution{
+public:
+    ListNode* merge(ListNode *left, ListNode *right){
+	ListNode *fakeHead = new ListNode(1);
+	ListNode *z = fakeHead;
+	while(left && right){
+	    if(left->val<right->val){
+		z->next = new ListNode(left->val);
+		z = z->next;
+		left = left->next;
+	    }
+	    else{
+		z->next = new ListNode(right->val);
+		z = z->next;
+		right = right->next;
+	    }
+	}
 
-ListNode* merge(ListNode *left, ListNode *right){
-    ListNode *z = NULL;
-    while(left && right){
-	if(left->val<right->val){
-	    push_back(&z, left->val);
+	while(left){
+	    z->next = new ListNode(left->val);
+	    z = z->next;
 	    left = left->next;
 	}
-	else{
-	    push_back(&z, right->val);
+
+	while(right){
+	    z->next = new ListNode(right->val);
+	    z = z->next;
 	    right = right->next;
 	}
+
+	return fakeHead->next;
     }
 
-    while(left){
-	push_back(&z, left->val);
-	left = left->next;
+    ListNode* getMid(ListNode* head){
+	ListNode *slow, *fast;
+	if(!head||!head->next || !head->next->next){
+	    return head;
+	}
+
+	for(slow=head, fast = head; fast->next && fast->next->next; slow=slow->next,fast = fast->next->next){
+
+	}
+	return slow;
     }
 
-    while(right){
-	push_back(&z, right->val);
-	right = right->next;
-    }
-
-    return z;
-}
-
-ListNode* getMid(ListNode* head){
-    ListNode *slow, *fast;
-    if(!head||!head->next || !head->next->next){
-	return head;
-    }
-
-    for(slow=head, fast = head; fast->next && fast->next->next; slow=slow->next,fast = fast->next->next){
-
-    }
-    return slow;
-}
-
-ListNode *mergeSort(ListNode *head){
-    if(!head || !head->next)
-	return head;
-    ListNode *mid = getMid(head);
-    ListNode *right = mid->next;
-    mid->next = NULL;
+    ListNode *mergeSort(ListNode *head){
+	if(!head || !head->next)
+	    return head;
+	ListNode *mid = getMid(head);
+	ListNode *right = mid->next;
+	mid->next = NULL;
     
-    return merge(mergeSort(head), mergeSort(right));
-}
-ListNode *sortList(ListNode *head) {
-    return mergeSort(head);
-}
+	return merge(mergeSort(head), mergeSort(right));
+    }
+    ListNode *sortList(ListNode *head) {
+	return mergeSort(head);
+    }
+
+
+};
 
 int main(int argc, char** argv){
     srand(time(NULL));
-    int n = 10;
-    ListNode *head = NULL;
+    int n;
+    if(argc>1)
+	n = atoi(argv[1]);
+    else
+	n = 100;
+    ListNode *head = new ListNode(1);
+    ListNode *x = head;
     For(i,0,n){
-	push_back(&head, rand()%(n*n));
+	x->next = new ListNode(rand()%(n*n));
+	x = x->next;
     }
+    head = head->next;
 
-    cout <<endl;
-    head = mergeSort(head);
+    Solution* sol = new Solution();
+
+    head = sol->sortList(head);
     for(ListNode* it = head; it; it = it->next){
 	cout << it->val <<" ";
     }
