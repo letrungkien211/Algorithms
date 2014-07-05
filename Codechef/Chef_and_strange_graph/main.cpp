973db7bd224755d426028b17cc15a2353320cdd1
letrungkien211 <letrungkien211@gmail.com>
2014-07-05 12:11:59 +0900

    Backup July

---
+++ Codechef/Chef_and_strange_graph/main.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+
+
+
+int main(int argc, char**argv)
+{
+    clock_t start = clock();
+
+    cout << "Time: " << (clock()-(double)start)/(double)CLOCKS_PER_SEC << endl;
+    
+    return 0;
+
+}
--- Template/template.cpp
+++ Template/template.cpp
@@ -1,79 +1,34 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<set>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <list>
 #include <map>
+#include <set>
+#include <deque>
 #include <queue>
-#include<unordered_set>
-
+#include <stack>
+#include <bitset>
+#include <sstream>
+#include <fstream>
 using namespace std;
 
-class Solution {
-public:
+int main(int argc, char**argv)
+{
+    clock_t start = clock();
 
-    vector<vector<string>> getPaths(string start, string end,
-	unordered_map<string, unordered_set<string>> backtrack, unordered_set<string> dict){
-	    vector<vector<string>> results;
-	    unordered_set<string> curr_track = backtrack[end];
-	    if(backtrack[end].empty())
-	        return results;
-	    for(unordered_set<string>::iterator it = curr_track.begin(); it!=curr_track.end(); it++){
-	        results = getPaths(start,*it, backtrack, dict);
-	        for(int i =0; i < results.size(); i++)
-	            results[i].push_back(end);
-	    }
-	    return results;  
-	    
-	}
 
-    vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
-        queue <string > open;
-        unordered_set<string> closed;
-        unordered_set<string> levelClosed;
-        unordered_map<string,unordered_set<string>> backtrack;
+    cout << "Time: " << (clock()-(double)start)/(double)CLOCKS_PER_SEC << endl;
+    
+    return 0;
 
-		if (start == end){
-		    return  vector<vector<string>>{vector<string>{start}};
-		}			
-		open.push(start);
-		closed.insert(start);
-		
-		int minLength = numeric_limits<int>::max();
-		int length = 0;
-		bool flag = false;
-		while (!open.empty()){
-		    if(length>=minLength)
-		        break;
-		    string curr = open.front();
-		    string hold_curr = curr;
-		    open.pop();
-		    for(int i =0; i<curr.size(); i++){
-		      char tmp = curr[i];
-		          for (char c = 'a'; c <= 'z'; c++){
-		            if(c==tmp)
-		            	continue;
-    				curr[i] = c;
-    				if (curr == end){
-    				    minLength = min(minLength,length+1);
-    					backtrack[end].insert(hold_curr);
-    					break;
-    				}
-					else if (closed.find(curr)==closed.end() && dict.find(curr) != dict.end()){
-        				    backtrack[curr].insert(hold_curr);
-        				    levelClosed.insert(curr);
-					}
-		            curr[i]=tmp;
-		        }
-		    }
-		    
-		    if(!open.empty()){
-		        for(auto s : levelClosed){
-		            open.push(s);
-		            closed.insert(s);
-		            length++;
-		        }
-		    }
-		}
-		return getPaths(start, end, backtrack, dict);
-	}
-};
+}
