ee4455b498149504d93bb136d62f4333b66e460f
letrungkien211 <letrungkien211@gmail.com>
2014-08-09 00:19:48 +0900

    Not good version

--- Coursera/AlgorithmDesign2/5.cpp
+++ Coursera/AlgorithmDesign2/5.cpp
@@ -17,12 +17,27 @@ public:
     double x,y;
 };
 
+namespace std{
+    template<>    
+    struct hash<list<int>> {
+	size_t operator() (const list<int> &l) const
+	{
+	    size_t ret = 0;
+	    for(auto i:l)
+		ret^=hash<int>() (i);
+	    return ret;
+	}
+    };
+}
+
+#define INF numeric_limits<double>::max()
+
 class TSP{
 public:
     vector<Point> points;
     vector<vector<double> > distances;
     vector<unordered_map<list<int>,double> > memo;
-    vector<list<int> > subsets;
+    vector<list<list<int> > > subsets;
     int N;
     
     void ReadFromFile(string filename){
@@ -41,6 +56,39 @@ public:
 		distances[i][j] = sqrt(pow(points[i].x-points[j].x,2) + pow(points[i].y-points[j].y,2));
 	    }
 	}
+	list<int> curr{1};
+	subsets.resize(N+1);
+	memo.resize(N+1);
+	GenerateSubset(2,1,curr);
+
+	list<int> temp{1,1};
+	memo[1][temp]=0;
+	for(int i=2; i<=N; i++){
+	    temp.back()= i;
+	    memo[1][temp] = INF;
+	}
+    }
+    
+    void GenerateSubset(int start, int depth, list<int>& curr){
+	subsets[depth].push_back(curr);
+	if(depth>=N)
+	    return;
+	for(int i =start; i<=N; i++){
+	    curr.push_back(i);
+	    GenerateSubset(i+1, depth+1, curr);
+	    curr.pop_back();
+	}
+    }
+    
+    void RunAlgorithm(){
+	for(int m=2; m<=N; m++){
+	    for(auto subset: subsets[m]){
+
+		for(auto i:
+		    
+		}
+	    }
+	}
     }
 };
 
@@ -48,7 +96,12 @@ public:
 
 int main(int argc, char** argv){
     clock_t start = clock();
-
+    TSP tsp;
+    cout << "Read File" <<endl;
+    tsp.ReadFromFile(argv[1]);
+    cout << "Preprocess" <<endl;
+    tsp.Preprocess();
+    cout << "Run" <<endl;
 
     cout << "Elapsed time: " << ((double)clock()-(double)start)/(double)CLOCKS_PER_SEC <<endl;
     return 0;
